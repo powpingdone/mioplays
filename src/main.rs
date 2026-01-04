1f5547f3dd0172d1ba023474bf7467abdb95eadd
Aidan <aidanzcase@gmail.com>
2026-01-04 15:43:03 -0600

    add some rudimentary albumview

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,20 @@ slint::include_modules!();
 fn main() {
     let mainui = MainWindow::new().unwrap();
     let browse_state = mainui.global::<MainBrowsingState>();
-    let e = Rc::new(slint::VecModel::from(vec![AlbumItem {
-        album: "Album Mommy".into(),
-        artist: "Artist Ipsum".into(),
-        id: 0,
-        title: "Title Lorem".into(),
-        album_art: slint::Image::load_from_path(&PathBuf::from("../img.jpg")).unwrap(),
-    }]));
-    browse_state.set_tracks(slint::ModelRc::from(e));
+    let e = slint::ModelRc::new(slint::VecModel::from(
+        vec![vec![AlbumItem {
+            album: "Album Mommy".into(),
+            artist: "Artist Ipsum".into(),
+            id: 0,
+            title: "Title Lorem".into(),
+            album_art: slint::Image::load_from_path(&PathBuf::from("../img.jpg")).unwrap(),
+        }]]
+        .into_iter()
+        .map(|inner| slint::ModelRc::new(slint::VecModel::from(inner)))
+        .collect::<Vec<_>>(),
+    ));
+
+    browse_state.set_tracks(e);
     drop(browse_state);
     mainui.run().unwrap();
 }
