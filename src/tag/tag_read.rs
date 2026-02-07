d92a0e5c170ffa582d1ce4f294981235c0f5ad92
Aidan <aidanzcase@gmail.com>
2026-02-07 16:45:24 -0600

    finish tag parsing

--- src/tag/tag_read.rs
+++ src/tag/tag_read.rs
@@ -1,6 +1,7 @@
-use std::{num::NonZeroUsize, ops::Deref, path::PathBuf, sync::LazyLock};
+use std::{path::PathBuf, sync::LazyLock};
 
-use lofty::{file::TaggedFileExt, tag::ItemKey};
+use lofty::prelude::*;
+use lofty::tag::ItemKey;
 use smol::lock::Semaphore;
 
 use crate::tag::tag_set;
@@ -17,151 +18,158 @@ pub async fn decode_tags(inp: PathBuf) -> tag_set::TagSet {
         )
     });
 
-    // TODO: impl
+    // spawn worker thread
     let _lock = READING_THREADS.acquire().await;
     let (tx, rx) = oneshot::channel();
-    std::thread::spawn(move || {
-        let mut ret = tag_set::TagSet::new();
-        // probe the item
-        let probe = lofty::probe::Probe::open(inp)
-            .expect("file cannot be opened")
-            .options(
-                lofty::config::ParseOptions::new()
-                    .parsing_mode(lofty::config::ParsingMode::Relaxed)
-                    .max_junk_bytes(4096)
-                    .read_cover_art(true)
-                    .read_tags(true)
-                    .read_properties(false),
-            )
-            .guess_file_type()
-            .expect("unable to guess file type")
-            .read()
-            .expect("unable to read file tags");
+    std::thread::spawn(move || tag_probe_worker_thread(inp, tx));
 
-        for tag in probe.tags() {
-            for item in tag.items() {
-                // TODO: possibly use item.lang()
-                let (k, v) = (item.key(), item.value());
-                let tag = match k {
-                    ItemKey::AlbumTitle => todo!(),
-                    ItemKey::SetSubtitle => todo!(),
-                    ItemKey::ShowName => todo!(),
-                    ItemKey::ContentGroup => todo!(),
-                    ItemKey::TrackTitle => todo!(),
-                    ItemKey::TrackSubtitle => todo!(),
-                    ItemKey::OriginalAlbumTitle => todo!(),
-                    ItemKey::OriginalArtist => todo!(),
-                    ItemKey::OriginalLyricist => todo!(),
-                    ItemKey::AlbumTitleSortOrder => todo!(),
-                    ItemKey::AlbumArtistSortOrder => todo!(),
-                    ItemKey::TrackTitleSortOrder => todo!(),
-                    ItemKey::TrackArtistSortOrder => todo!(),
-                    ItemKey::ShowNameSortOrder => todo!(),
-                    ItemKey::ComposerSortOrder => todo!(),
-                    ItemKey::AlbumArtist => todo!(),
-                    ItemKey::TrackArtist | ItemKey::TrackArtists => todo!(),
-                    ItemKey::Arranger => todo!(),
-                    ItemKey::Writer => todo!(),
-                    ItemKey::Composer => todo!(),
-                    ItemKey::Conductor => todo!(),
-                    ItemKey::Director => todo!(),
-                    ItemKey::Engineer => todo!(),
-                    ItemKey::Lyricist => todo!(),
-                    ItemKey::MixDj => todo!(),
-                    ItemKey::MixEngineer => todo!(),
-                    ItemKey::MusicianCredits => todo!(),
-                    ItemKey::Performer => todo!(),
-                    ItemKey::Producer => todo!(),
-                    ItemKey::Publisher => todo!(),
-                    ItemKey::Label => todo!(),
-                    ItemKey::InternetRadioStationName => todo!(),
-                    ItemKey::InternetRadioStationOwner => todo!(),
-                    ItemKey::Remixer => todo!(),
-                    ItemKey::DiscNumber => todo!(),
-                    ItemKey::DiscTotal => todo!(),
-                    ItemKey::TrackNumber => todo!(),
-                    ItemKey::TrackTotal => todo!(),
-                    ItemKey::Popularimeter => todo!(),
-                    ItemKey::ParentalAdvisory => todo!(),
-                    ItemKey::RecordingDate => todo!(),
-                    ItemKey::Year => todo!(),
-                    ItemKey::ReleaseDate => todo!(),
-                    ItemKey::OriginalReleaseDate => todo!(),
-                    ItemKey::Isrc => todo!(),
-                    ItemKey::Barcode => todo!(),
-                    ItemKey::CatalogNumber => todo!(),
-                    ItemKey::Work => todo!(),
-                    ItemKey::Movement => todo!(),
-                    ItemKey::MovementNumber => todo!(),
-                    ItemKey::MovementTotal => todo!(),
-                    ItemKey::MusicBrainzRecordingId => todo!(),
-                    ItemKey::MusicBrainzTrackId => todo!(),
-                    ItemKey::MusicBrainzReleaseId => todo!(),
-                    ItemKey::MusicBrainzReleaseGroupId => todo!(),
-                    ItemKey::MusicBrainzArtistId => todo!(),
-                    ItemKey::MusicBrainzReleaseArtistId => todo!(),
-                    ItemKey::MusicBrainzWorkId => todo!(),
-                    ItemKey::FlagCompilation => todo!(),
-                    ItemKey::FlagPodcast => todo!(),
-                    ItemKey::FileType => todo!(),
-                    ItemKey::FileOwner => todo!(),
-                    ItemKey::TaggingTime => todo!(),
-                    ItemKey::Length => todo!(),
-                    ItemKey::OriginalFileName => todo!(),
-                    ItemKey::OriginalMediaType => todo!(),
-                    ItemKey::EncodedBy => todo!(),
-                    ItemKey::EncoderSoftware => todo!(),
-                    ItemKey::EncoderSettings => todo!(),
-                    ItemKey::EncodingTime => todo!(),
-                    ItemKey::ReplayGainAlbumGain => todo!(),
-                    ItemKey::ReplayGainAlbumPeak => todo!(),
-                    ItemKey::ReplayGainTrackGain => todo!(),
-                    ItemKey::ReplayGainTrackPeak => todo!(),
-                    ItemKey::AudioFileUrl => todo!(),
-                    ItemKey::AudioSourceUrl => todo!(),
-                    ItemKey::CommercialInformationUrl => todo!(),
-                    ItemKey::CopyrightUrl => todo!(),
-                    ItemKey::TrackArtistUrl => todo!(),
-                    ItemKey::RadioStationUrl => todo!(),
-                    ItemKey::PaymentUrl => todo!(),
-                    ItemKey::PublisherUrl => todo!(),
-                    ItemKey::Genre => todo!(),
-                    ItemKey::InitialKey => todo!(),
-                    ItemKey::Color => todo!(),
-                    ItemKey::Mood => todo!(),
-                    ItemKey::Bpm => todo!(),
-                    ItemKey::IntegerBpm => todo!(),
-                    ItemKey::CopyrightMessage => todo!(),
-                    ItemKey::License => todo!(),
-                    ItemKey::PodcastDescription => todo!(),
-                    ItemKey::PodcastSeriesCategory => todo!(),
-                    ItemKey::PodcastUrl => todo!(),
-                    ItemKey::PodcastGlobalUniqueId => todo!(),
-                    ItemKey::PodcastKeywords => todo!(),
-                    ItemKey::Comment => todo!(),
-                    ItemKey::Description => todo!(),
-                    ItemKey::Language => todo!(),
-                    ItemKey::Script => todo!(),
-                    ItemKey::Lyrics => todo!(),
-                    ItemKey::AppleXid => todo!(),
-                    ItemKey::AppleId3v2ContentGroup => todo!(),
-
-                    ItemKey::Unknown(item) => {
-                        ret.push_custom_tag(item, Box::new(v.into_binary().unwrap()));
-                        continue;
-                    }
-                    _ => unreachable!(),
-                };
-                ret.push_typed_tag(tag);
-            }
-        }
-        tx.send(ret)
-            .expect("the outer thread closed the recv, and therefore we cannot send the tags");
-    });
-
+    // await return
     let ret = rx
-        .recv()
+        .await
         .expect("the inner thread did not return, and instead dropped the oneshot");
     drop(_lock);
     ret
+}
+
+// boilerplate macros to set fields on the tag structs
+macro_rules! set_string {
+    ($map:ident, $key:ty {$value:ident: $field:ident}) => {{
+        // check if we can extract a string
+        if let Some(value) = $value.text() {
+            let value = value.to_owned();
+            // try getting a mut ref to the tagmap
+            let tag = $map.get_typed_tag_mut::<$key>();
+            match tag {
+                Some(tag) => {
+                    // if exists, set field
+                    tag.$field = Some(value);
+                }
+                None => {
+                    // if not, create from default
+                    //
+                    // this is weird syntax for what I want, but tldr
+                    // I can't just $map.push_typed_tag($key { inner });
+                    // so I manually specify from the default, then modify it.
+                    let mut inp: $key = Default::default();
+                    inp.$field = Some(value);
+                    drop($map.push_typed_tag(inp));
+                }
+            }
+        }
+    }};
+}
+
+macro_rules! push_string {
+    ($map:ident, $key:ty {$value:ident: $field:ident}) => {{
+        // read set_string above for comments.
+        // the only difference is that this expects to operate on a vec
+        if let Some(value) = $value.text() {
+            let value = value.to_owned();
+            let tag = $map.get_typed_tag_mut::<$key>();
+            match tag {
+                Some(tag) => {
+                    tag.$field.push(value);
+                }
+                None => {
+                    let mut inp: $key = Default::default();
+                    inp.$field.push(value);
+                    drop($map.push_typed_tag(inp));
+                }
+            }
+        }
+    }};
+}
+
+macro_rules! set_parsed {
+        ($map:ident, $key:ty {$value:ident: $field:ident}) => {{
+            // check if key exists
+            let check_key = $map.drop_typed_tag::<$key>();
+            if check_key.is_none()
+                // check if string is parseable and that it exists
+                && let Some(text) = $value.text()
+                && let Ok(parsed) = text.parse()
+            {
+                // set the tag
+                let mut inp: $key = Default::default();
+                inp.$field = parsed;
+                drop($map.push_typed_tag(inp));
+            }
+        }};
+    }
+
+/// The worker thread for extracting the item. While this is supposted to be async,
+/// because of lofty not being async itself, just use this worker thread that can
+/// block instead.
+fn tag_probe_worker_thread(inp: PathBuf, tx: oneshot::Sender<tag_set::TagSet>) {
+    use crate::tag;
+
+    let mut map = tag_set::TagSet::new();
+
+    // probe the item
+    let probe = lofty::probe::Probe::open(inp)
+        .expect("file cannot be opened")
+        .options(
+            lofty::config::ParseOptions::new()
+                .parsing_mode(lofty::config::ParsingMode::Relaxed)
+                .max_junk_bytes(4096)
+                .read_cover_art(true)
+                .read_tags(true)
+                .read_properties(false),
+        )
+        .guess_file_type()
+        .expect("unable to guess file type")
+        .read()
+        .expect("unable to read file tags");
+
+    for tag in probe.tags() {
+        // string tags
+        for item in tag.items() {
+            // TODO: possibly use item.lang()
+            let (key, value) = (item.key(), item.value());
+            match key {
+                // typed items
+                ItemKey::AlbumTitle => set_string!(map, tag::AlbumTitle { value: inner }),
+                ItemKey::AlbumTitleSortOrder => {
+                    set_string!(map, tag::AlbumTitle { value: sort_order })
+                }
+                ItemKey::AlbumArtist => push_string!(map, tag::AlbumArtist { value: inner }),
+                ItemKey::AlbumArtistSortOrder => {
+                    push_string!(map, tag::AlbumArtist { value: sort_order })
+                }
+                ItemKey::Composer => set_string!(map, tag::Composer { value: inner }),
+                ItemKey::ComposerSortOrder => {
+                    set_string!(map, tag::Composer { value: sort_order })
+                }
+                ItemKey::DiscNumber => set_parsed!(map, tag::DiscPos { value: inner }),
+                ItemKey::DiscTotal => set_parsed!(map, tag::DiscTotal { value: inner }),
+                ItemKey::Performer => push_string!(map, tag::Performer { value: inner }),
+                ItemKey::TrackArtist | ItemKey::TrackArtists => {
+                    push_string!(map, tag::TrackArtist { value: inner })
+                }
+                ItemKey::TrackArtistSortOrder => {
+                    push_string!(map, tag::TrackArtist { value: sort_order })
+                }
+                ItemKey::TrackNumber => set_parsed!(map, tag::TrackPos { value: inner }),
+                ItemKey::TrackTitle => set_string!(map, tag::TrackTitle { value: inner }),
+                ItemKey::TrackTitleSortOrder => {
+                    set_string!(map, tag::TrackTitle { value: sort_order })
+                }
+                ItemKey::TrackTotal => set_parsed!(map, tag::TrackTotal { value: inner }),
+                ItemKey::ReleaseDate => set_parsed!(map, tag::ReleaseDate { value: inner }),
+                ItemKey::Remixer => push_string!(map, tag::Remixer { value: inner }),
+                _ => continue,
+            }
+        }
+
+        // cover art
+        if let Some(img) = tag.get_picture_type(lofty::picture::PictureType::CoverFront)
+            && map.get_typed_tag::<tag::EncodedCoverArt>().is_none()
+        {
+            let buf = img.data().to_owned().into();
+            drop(map.push_typed_tag(tag::EncodedCoverArt(buf)));
+        }
+    }
+
+    tx.send(map)
+        .expect("the outer thread closed the recv, and therefore we cannot send the tags");
 }
--- src/tag/tag_set.rs
+++ src/tag/tag_set.rs
@@ -1,7 +1,7 @@
 use std::{
     any::{Any, TypeId},
     collections::HashMap,
-    fmt::{self, Debug},
+    fmt::Debug,
 };
 
 /// `Tag`: a sealed marker trait for interacting with the `TagMap` in a
@@ -11,21 +11,25 @@ mod private {
 }
 pub trait Tag: private::Sealed + Any + Debug {
     fn to_any(&self) -> &(dyn Any + 'static);
+    fn to_any_mut(&mut self) -> &mut (dyn Any + 'static);
     fn to_any_boxed(self: Box<Self>) -> Box<dyn Any + 'static>;
+    fn to_debug(&self) -> &(dyn Debug + 'static);
     fn display_name(&self) -> Option<&str>;
 }
 macro_rules! tag_impl {
     ($tag:ident {$($name:ident : $typ:ty),+} => $display_name:expr) => {
-        #[derive(Debug)]
+        #[derive(Debug, Default)]
         pub struct $tag {
-            $( $name: $typ, )+
+            $( pub $name: $typ, )+
         }
 
         tag_trait_impl!($tag: $display_name);
     };
     ($tag:ident as $inner:ty => $display_name:expr) => {
-        #[derive(Debug)]
-        pub struct $tag(pub $inner);
+        #[derive(Debug, Default)]
+        pub struct $tag{
+            pub inner: $inner,
+        }
 
         tag_trait_impl!($tag: $display_name);
     };
@@ -39,7 +43,15 @@ macro_rules! tag_trait_impl {
                 self
             }
 
+            fn to_any_mut(&mut self) -> &mut (dyn Any + 'static) {
+                self
+            }
+
             fn to_any_boxed(self: Box<Self>) -> Box<dyn Any + 'static> {
+                self
+            }
+
+            fn to_debug(&self) -> &(dyn Debug + 'static) {
                 self
             }
 
@@ -57,17 +69,16 @@ macro_rules! tag_trait_impl {
 // * any tags that holds an artist name is expected to hold multiple artist names
 //
 // * any tags that involve sort order will insert it into the struct of the same name
-// (AlbumTitleSortOrder -> AlbumTitle {inner: String, sort_order: Option<String>})
+// (AlbumTitleSortOrder -> AlbumTitle {inner: String, sort_order: String})
 
 // Titles/Subtitles
-tag_impl!(AlbumTitle {inner: String, sort_order: String} => "Album Title");
-tag_impl!(TrackTitle {inner: String, sort_order: String} => "Track Title");
-tag_impl!(DiscTitle as String => "Disc Title"); // title of individual disk
+tag_impl!(AlbumTitle {inner: Option<String>, sort_order: Option<String>} => "Album Title");
+tag_impl!(TrackTitle {inner: Option<String>, sort_order: Option<String>} => "Track Title");
 
 // Creators (Artists) & Credits
 tag_impl!(AlbumArtist {inner: Vec<String>, sort_order: Vec<String>} => "Album Artist");
 tag_impl!(TrackArtist {inner: Vec<String>, sort_order: Vec<String>} => "Track Artist");
-tag_impl!(Composer {inner: String, sort_order: String} => "Composer");
+tag_impl!(Composer {inner: Option<String>, sort_order: Option<String>} => "Composer");
 tag_impl!(Performer as Vec<String> => "Performer");
 tag_impl!(Remixer as Vec<String> => "Remixer");
 
@@ -98,14 +109,23 @@ tag_impl!(ReleaseDate as jiff::Timestamp => "Release Date");
 
 // Special tag for the string insert
 #[derive(Debug)]
-pub struct UnknownItem(Box<dyn Any + Send + Sync + 'static>);
+#[allow(unused)]
+pub struct UnknownItem(pub Box<dyn Any + Send + Sync + 'static>);
 impl private::Sealed for UnknownItem {}
 impl Tag for UnknownItem {
     fn to_any(&self) -> &(dyn Any + 'static) {
         self
     }
 
+    fn to_any_mut(&mut self) -> &mut (dyn Any + 'static) {
+        self
+    }
+
     fn to_any_boxed(self: Box<Self>) -> Box<dyn Any + 'static> {
+        self
+    }
+
+    fn to_debug(&self) -> &(dyn Debug + 'static) {
         self
     }
 
@@ -116,14 +136,22 @@ impl Tag for UnknownItem {
 
 // Special tag for the Cover Art
 #[derive(Debug)]
-pub struct EncodedCoverArt(Box<[u8]>);
+pub struct EncodedCoverArt(pub Box<[u8]>);
 impl private::Sealed for EncodedCoverArt {}
 impl Tag for EncodedCoverArt {
     fn to_any(&self) -> &(dyn Any + 'static) {
         self
     }
 
+    fn to_any_mut(&mut self) -> &mut (dyn Any + 'static) {
+        self
+    }
+
     fn to_any_boxed(self: Box<Self>) -> Box<dyn Any + 'static> {
+        self
+    }
+
+    fn to_debug(&self) -> &(dyn Debug + 'static) {
         self
     }
 
@@ -164,13 +192,46 @@ impl From<&str> for TIDOrCustom {
 /// Fetching a typed item will return it's associated struct, while a
 /// string will only ever return a `dyn Tag` which can be cast into a
 /// `dyn Any` using the `Tag::to_any` and `Tag::to_any_boxed` methods.
-#[derive(Debug)]
 pub struct TagSet {
     map: HashMap<TIDOrCustom, Box<dyn Tag + Send + Sync + 'static>>,
 }
 
+// due to the custom nature of tagset, this debug handler allows for formatting
+// and indicating custom tags, as well as emitting the .to_debug() items
+impl Debug for TagSet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        /// Helper type for Custom tags
+        struct CustomDebug<'a, 'b> {
+            name: &'a str,
+            debug: &'b dyn Debug,
+        }
+
+        impl<'a, 'b> Debug for CustomDebug<'a, 'b> {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_tuple(&format!("CustomTag: {}", self.name))
+                    .field(self.debug)
+                    .finish()
+            }
+        }
+
+        let mut f_owned = f.debug_set();
+        let mut f = &mut f_owned;
+        for (ref k, ref v) in self.map.iter() {
+            f = match k {
+                TIDOrCustom::TypeId(_) => f.entry(v.to_debug()),
+                TIDOrCustom::Custom(name) => f.entry(&CustomDebug {
+                    name,
+                    debug: v.to_debug(),
+                }),
+            };
+        }
+        f.finish()
+    }
+}
+
 // Not related to accesses.
 impl TagSet {
+    /// Create a new tagset.
     pub fn new() -> Self {
         Self {
             map: HashMap::new(),
@@ -200,27 +261,36 @@ impl TagSet {
         self.map.get(&type_id.into())?.to_any().downcast_ref()
     }
 
+    /// Fetch a mutable reference to a typed `Tag`.
+    pub fn get_typed_tag_mut<K: Tag + Send + Sync + 'static>(&mut self) -> Option<&mut K> {
+        let type_id = TypeId::of::<K>();
+        self.map
+            .get_mut(&type_id.into())?
+            .to_any_mut()
+            .downcast_mut()
+    }
+
     /// Fetch and return a typed `Tag`, removing it from the `TagMap`.
     pub fn drop_typed_tag<K: Tag + Send + Sync + 'static>(&mut self) -> Option<K> {
         let type_id = TypeId::of::<K>();
-        let ret = self
-            .map
-            .remove(&type_id.into())?
-            .to_any_boxed()
-            .downcast::<K>();
-        if ret.is_err() {
+        let removed = self.map.remove(&type_id.into())?;
+        let type_test = &removed.to_any().downcast_ref::<K>();
+        if type_test.is_none() {
             panic!(
-                "tag map type mismatch: expected {:?} (aka {}) but the key was not that",
+                "tag map type mismatch: expected {:?} (aka {}) but the key is {:?}",
                 type_id,
                 std::any::type_name::<K>(),
+                removed.to_debug(),
             );
         } else {
+            let ret = removed.to_any_boxed().downcast();
             Some(*ret.unwrap())
         }
     }
 }
 
 // Stringly accessing fields
+#[allow(unused)]
 impl TagSet {
     /// Add tag to set which does not have an associated type, but a custom string.
     /// If the associated tag is already in the set, return the argument as an error.
@@ -248,6 +318,14 @@ impl TagSet {
         key: impl AsRef<str>,
     ) -> Option<&Box<dyn Tag + Send + Sync + 'static>> {
         self.map.get(&key.as_ref().into())
+    }
+
+    /// Fetch a mutable reference to an associated custom tag object
+    pub fn get_custom_tag_mut(
+        &mut self,
+        key: impl AsRef<str>,
+    ) -> Option<&mut Box<dyn Tag + Send + Sync + 'static>> {
+        self.map.get_mut(&key.as_ref().into())
     }
 
     /// Fetch and return an associated custom tag object, removing it from the `TagMap`.
